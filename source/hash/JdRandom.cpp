ec45559f37b3dae182f37926c2c282b448d62a7c
Steven Massey <smassey@Stevens-MacBook-Pro-2.local>
2017-11-25 17:39:02 -0800

    no message

---
+++ source/hash/JdRandom.cpp
@@ -0,0 +1,11 @@
+//
+//  JdRandom.cpp
+//  Jigidesign
+//
+//  Created by Steven Massey on 5/13/16.
+//  Copyright © 2016 Jigidesign. All rights reserved.
+//
+
+#include "JdRandom.hpp"
+
+JdRandom::Generator64 JdRandom::s_64;
---
+++ source/hash/JdRandom.hpp
@@ -0,0 +1,54 @@
+//
+//  JdRandom.hpp
+//  Jigidesign
+//
+//  Created by Steven Massey on 5/13/16.
+//  Copyright © 2016 Jigidesign. All rights reserved.
+//
+
+#ifndef JdRandom_hpp
+#define JdRandom_hpp
+
+#include "JdNucleus.hpp"
+#include <random>
+
+class JdRandom
+{
+	public:
+	
+	template <typename T>
+	static T Get ()
+	{
+		return s_64.Generate ();
+	}
+	
+
+	template <typename T>
+	static T GetPositive ()
+	{
+		return s_64.Generate () & 0x7fffffffffffffff;
+	}
+
+	protected:
+	struct Generator64
+	{
+		u64 Generate ()
+		{
+			lock_guard <mutex> lock (m_lock);
+			
+			return dis (gen);
+		}
+		
+		std::random_device rd;
+		std::mt19937_64 gen { rd() };
+		
+		std::uniform_int_distribution <u64> dis;
+
+		mutex m_lock;
+	};
+	
+	static Generator64 s_64;
+};
+
+
+#endif /* JdRandom_hpp */
