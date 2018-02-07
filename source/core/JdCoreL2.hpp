3688ad297066d2a55650c492655eb4ceac0628ff
Steven Massey <smassey@Stevens-MacBook-Pro-2.local>
2018-02-06 18:20:47 -0800

    no message

--- source/core/JdCoreL2.hpp
+++ source/core/JdCoreL2.hpp
@@ -18,6 +18,38 @@ namespace Jd
 {
 	i32 HashCString31 (const char *i_string);
 	u64 HashString64 (const string & i_string);
+
+	
+	template <class X>
+	X* SingletonHelper (bool i_create)
+	{
+		static X * lonely = new X;
+		static i64 usageCount = 0;
+		
+		if (i_create)
+		{
+			++usageCount;
+		}
+		else
+		{
+			if (--usageCount <= 0)
+			{
+				delete lonely;
+				lonely = nullptr;
+				usageCount = 0;
+			}
+		}
+		
+		return lonely;
+	}
+	
+	
+	template <class X>
+	X* AcquireSingleton () // X * should be subsequently released with the ReleaseSingleton () call, if you want the singleton to be eventually shutdown
+	{
+		return SingletonHelper <X> (true);
+	}
 }
+
 
 #endif /* defined(__Jigidesign__JdCoreL2__) */
