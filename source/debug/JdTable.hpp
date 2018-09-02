1ec491cdbf091f34a845d554c3e998e574e5a649
Steven Massey <smassey@MacBook-Crap.local>
2018-09-01 23:13:30 -0700

    JdTable PrintRow ()

--- source/debug/JdTable.hpp
+++ source/debug/JdTable.hpp
@@ -18,18 +18,25 @@ struct JdTable
 		JdTable table ({ "column1", ".:column2", ":column3", "L:column4"}); 	// fill column2 whitespace with '.' and  align column4 to the left
 		table.AddRow (w, x, y, z); 												// Jd::ToString () is applied on each element
 		table.Print (); 														// optional.  ~JdTable() prints by default.
+	
+		// or
+		JdTable table ({ "column1,10", ".:column2,20", ":column3,5", "L:column4,9"});
 	#endif
 	
 	struct Column
 	{
-		size_t				minWidth	= 2;
+		size_t				width		= 2;
 		char				fill		= ' ';
 		bool				alignLeft	= false;
 		
-		size_t				width		= 0;
-		
 		vector <string>		rows;
 	};
+	
+	~ JdTable ()
+	{
+		Print ();
+	}
+	
 	
 	JdTable (const initializer_list <string> & i_columnNames)
 	{
@@ -53,8 +60,46 @@ struct JdTable
 				name = name.substr (colon + 1);
 			}
 			
+			auto comma = name.find (",");
+			if (comma != string::npos)
+			{
+				string number = name.substr (comma + 1);
+				
+				column.width = stoll (number);
+				name = name.substr (0, comma);
+			}
+
 			column.rows.push_back (name);
 		}
+	}
+	
+	template <typename T, typename... Args>
+	void PrintRow (T && i_column, Args... i_columns)
+	{
+		size_t padding = 4;
+		size_t width = CalculateWidth (padding);
+		string div = GetDivider (width);
+
+		if (not m_printed)
+		{
+			cout << div;
+			OutputRow (0, padding);
+			cout << div;
+			m_printed = true;
+		}
+
+		for (auto & c : m_columns)
+			c.rows.clear ();
+
+		AddRow (i_column, i_columns...);
+		OutputRow (0, padding);
+	}
+	
+	void End ()
+	{
+		size_t padding = 4;
+		size_t width = CalculateWidth (padding);
+		cout << GetDivider (width);
 	}
 	
 	template <typename T, typename... Args>
@@ -77,60 +122,78 @@ struct JdTable
 		++m_numRows;
 	}
 	
-	void Print ()
+	protected:
+	
+	string GetDivider (size_t i_width)
 	{
-		size_t padding = 4;
+		string div (i_width + 4, '-');
+		div = "+" + div + "+";
+		div += "\n";
+		
+		return div;
+	}
+	
+	size_t CalculateWidth (size_t i_padding)
+	{
+		size_t padding = i_padding;
 		size_t totalWidth = -padding;
 		for (auto & c : m_columns)
 		{
-			size_t w = c.minWidth;
+			size_t & w = c.width;
 			
 			for (auto & r : c.rows)
 				w = max (r.size (), w);
 			
-			c.width = w;
-			
 			totalWidth += (c.width + padding);
 		}
 		
-		string div (totalWidth + 4, '-');
-		div = "+" + div + "+";
-		div += "\n";
-		
-		string pad (padding, ' ');
-		string headerPad = pad;
-//		headerPad [pad.size ()/2] = '|';
-		
-		for (size_t r = 0; r < m_numRows + 1; ++r)
-		{
-			if (r == 0) cout << div;
-			
-			cout << "|  ";
-			for (auto & c : m_columns)
-			{
-				d_jdAssert (r < c.rows.size (), "row out of bounds");
-				
-				if (r != 0) cout << setfill (c.fill);
-				cout << setw (c.width) << (c.alignLeft or r == 0 ? left : right) << c.rows [r];
-				cout << setfill (' ');
-				if (& c != & m_columns.back ())
-					cout << (r == 0 ? headerPad : pad);
-			}
-			cout << "  |\n";
-			
-			if (r == 0) cout << div;
-		}
-		
-		cout << div << endl;
-		
-		m_printed = true;
+		return totalWidth;
 	}
 	
 	
-	~ JdTable ()
+	void Print ()
 	{
-		if (not m_printed) Print ();
+		if (not m_printed)
+		{
+			size_t padding = 4;
+			size_t totalWidth = CalculateWidth (padding);
+			string div = GetDivider (totalWidth);
+			
+			cout << div;
+			for (size_t r = 0; r < m_numRows + 1; ++r)
+			{
+				OutputRow (r, padding);
+				if (r == 0) cout << div;
+			}
+			
+			cout << div << endl;
+			
+			m_printed = true;
+		}
 	}
+	
+	
+	void OutputRow (size_t i_row, size_t i_padding)
+	{
+		auto r = i_row;
+		
+		string pad (i_padding, ' ');
+		string headerPad = pad;
+
+		cout << "|  ";
+		for (auto & c : m_columns)
+		{
+			d_jdAssert (r < c.rows.size (), "row out of bounds");
+			
+			if (r != 0) cout << setfill (c.fill);
+			cout << setw (c.width) << (c.alignLeft or r == 0 ? left : right) << c.rows [r];
+			cout << setfill (' ');
+			if (& c != & m_columns.back ())
+				cout << (r == 0 ? headerPad : pad);
+		}
+		cout << "  |\n";
+	}
+	
 	
 	vector <Column>			 	m_columns;
 	bool						m_printed			= false;
