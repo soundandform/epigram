fa7280fb05973285862c08601d2558689a00549a
Steven Massey <smassey@MacBook-Crap.local>
2018-09-18 18:50:16 -0700

    no message

---
+++ source/utils/JdVersion.cpp
@@ -0,0 +1,98 @@
+//
+//  JdVersoin.cpp
+//
+//  Created by Steven Massey on 11/10/12.
+//  Copyright (c) 2012 Jigidesign. All rights reserved.
+//
+
+#include <stdio.h>
+#include "JdVersion.hpp"
+
+bool operator == (const JdVersion &i_versionA, const JdVersion &i_versionB)
+{
+	if (i_versionA.major != i_versionB.major) return false;
+	
+	if (i_versionA.minor != c_jdVersionNumDontCare && i_versionB.minor != c_jdVersionNumDontCare)
+	{
+		if (i_versionA.minor != i_versionB.minor) return false;
+	}
+	
+	if (i_versionA.revision != c_jdRevisionNumDontCare && i_versionB.revision != c_jdRevisionNumDontCare)
+	{
+		if (i_versionA.revision != i_versionB.revision) return false;
+	}
+	return true;
+}
+
+
+bool operator >= (const JdVersion &i_versionA, const JdVersion &i_versionB)
+{
+	if (i_versionA.major < i_versionB.major && i_versionB.major != c_jdVersionNumDontCare) return false;
+	
+	if (i_versionA.minor != c_jdVersionNumDontCare && i_versionB.minor != c_jdVersionNumDontCare)
+	{
+		if (i_versionA.minor < i_versionB.minor) return false;
+	}
+	
+	if (i_versionA.revision != c_jdRevisionNumDontCare && i_versionB.revision != c_jdRevisionNumDontCare)
+	{
+		if (i_versionA.revision < i_versionB.revision) return false;
+	}
+	
+	return true;
+}
+
+
+bool operator <= (const JdVersion &i_versionA, const JdVersion &i_versionB)
+{
+	if (i_versionA.major > i_versionB.major) return false;
+	
+	if (i_versionA.minor != c_jdVersionNumDontCare && i_versionB.minor != c_jdVersionNumDontCare)
+	{
+		if (i_versionA.minor > i_versionB.minor) return false;
+	}
+	
+	if (i_versionA.revision != c_jdRevisionNumDontCare && i_versionB.revision != c_jdRevisionNumDontCare)
+	{
+		if (i_versionA.revision > i_versionB.revision) return false;
+	}
+	
+	return true;
+}
+
+
+bool operator < (const JdVersion &i_versionA, const JdVersion &i_versionB)
+{
+	if (i_versionA.major >= i_versionB.major) return false;
+	
+	if (i_versionA.minor != c_jdVersionNumDontCare && i_versionB.minor != c_jdVersionNumDontCare)
+	{
+		if (i_versionA.minor >= i_versionB.minor) return false;
+	}
+	
+	if (i_versionA.revision != c_jdRevisionNumDontCare && i_versionB.revision != c_jdRevisionNumDontCare)
+	{
+		if (i_versionA.revision >= i_versionB.revision) return false;
+	}
+	
+	return true;
+}
+
+
+std::ostream& operator << (std::ostream &output, const JdVersion &i_version)
+{
+	if (i_version.major == c_jdVersionNumDontCare) output << "x";
+	else output << i_version.major;
+	
+	output << ".";
+	
+	if (i_version.minor == c_jdVersionNumDontCare) output << "x";
+	else output << i_version.minor;
+	
+	output << ".";
+	
+	if (i_version.revision == c_jdRevisionNumDontCare) output << "x";
+	else output << i_version.revision;
+	
+	return output;
+}
