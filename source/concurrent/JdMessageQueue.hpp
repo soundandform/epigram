9d49e2f058cea31b802273ab08784873e97b3053
Steven Massey <steven@epigramlabs.com>
2019-06-27 14:38:01 -0700

    no message

--- source/concurrent/JdMessageQueue.hpp
+++ source/concurrent/JdMessageQueue.hpp
@@ -10,8 +10,6 @@
 
 #include "JdAssert.hpp"
 #include "JdSemaphore.hpp"
-//#include "Epigram.hpp"
-//#include "JdTimer.hpp"
 
 #include <boost/interprocess/detail/atomic.hpp>
 #include <boost/interprocess/sync/interprocess_semaphore.hpp>
