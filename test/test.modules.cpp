5900e500919d9509d38fac91fdb0876589d04fee
Steven Massey <soundandform@gmail.com>
2020-06-10 13:56:04 -0700

    no message

---
+++ test/test.modules.cpp
@@ -0,0 +1,14 @@
+//
+//  test.modules.cpp
+//
+//  Created by Steven Massey on 6/9/20.
+//  Copyright © 2020 Steven Massey. All rights reserved.
+//
+
+#include "doctest.hpp"
+
+
+doctest ("modules.basic")
+{
+//	JdEnvironment env;
+}
