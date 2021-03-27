24b99b5b8d9879b64214b16ecf8011249a1ff44b
Steven Massey <soundandform@gmail.com>
2021-03-27 11:28:19 -0700

    doctest_disabled

--- test/doctest.hpp
+++ test/doctest.hpp
@@ -3,6 +3,11 @@
 #	include <doctest/doctest.h>
 #	define test_suite(NAME) TEST_SUITE(#NAME)
 #	define doctest(NAME) TEST_CASE(NAME)
+
+#	define doctest_disabled__(LINE) void DisabledTest##LINE ()
+#	define doctest_disabled_(LINE) doctest_disabled__(LINE)
+#	define doctest_disabled(NAME) doctest_disabled_(__LINE__)
+
 #	define subcase(...) SUBCASE(__VA_ARGS__)
 #	define expect(...) CHECK(__VA_ARGS__);
 
